1eabbf9a6ac9ac40f655eee3389fd1a391ec46da
Slick3k <brenev.v@gmail.com>
2021-02-11 15:11:59 +0300

    first commit

---
+++ hello world/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int exampleint = 50;
+            double exampledouble = 50.5;
+            Console.WriteLine("Hello World!");
+            string[] hellos = { "hallo", "hello", "ciao", "salve" };
+
+            //for (int i = 0; i < exampleint; i++)
+            int i = 0;
+            foreach (string hello in hellos)
+            {
+                Console.WriteLine(hello);
+                i++;
+                Console.WriteLine("вы продолжили" + i + "раз");
+            }
+            /*if (Console.ReadKey().Key == ConsoleKey.N)
+            {
+                Console.WriteLine("\nвы продолжили");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("\nнажми N");
+                Console.ReadKey();
+            }*/
+        }
+    }
+}
