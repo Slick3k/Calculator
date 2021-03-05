abebb42258775ce9c6c0c4d1465cc46f43e6f089
Slick3k <brenev.v@gmail.com>
2021-03-05 11:03:49 +0300

    task 2

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -6,65 +6,76 @@ namespace calculator_task2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("При использовании дробных чисел записывать через запятую");
             while (true)
             {
+
                 Console.WriteLine("Введите число, операцию и второе число через Enter.");
+                
                 try
                 {
 
-                    Console.WriteLine("Ответ: " Calculate(Convert.ToDouble(Console.ReadLine()), Convert.ToChar(Console.ReadLine()), Convert.ToDouble(Console.ReadLine())));
+                    Console.WriteLine("Ответ: "+ Calculate(Convert.ToDouble(Console.ReadLine()), Convert.ToChar(Console.ReadLine()), Convert.ToDouble(Console.ReadLine())));
                 }
-                catch
+                catch (FormatException)
                 {
-
+                    Console.WriteLine("Что-то на татарском");
+                    
                 }
+                
             }
 
         }
         static double Calculate(double x, char oper, double y)
         {
-
-            switch (oper)
+            if ((oper == '+') || (oper == '-') || (oper == '*') || (oper == '/') || (oper == '^'))
+                switch (oper)
+                {
+                    case '+':
+                        return add(x, y);
+                    case '-':
+                        return sub(x, y);
+                    case '*':
+                        return mul(x, y);
+                    case '/':
+                        return div(x, y);
+                    case '^':
+                        return pow(x, y);
+                    default:
+                        throw new Exception("Введено не действие");
+                }
+            else
             {
-                case '+';
-                    return add(x, y);
-                case '-';
-                    return sub(x, y);
-                case '*';
-                    return mul(x, y);
-                case '/';
-                    return div(x, y);
-                case '^';
-                    return pow(x.y);
+                Console.WriteLine("Введено не действие");
+                return 0;
             }
-
-
         }
-        double add(double x, double y)
+        static double add(double x, double y)
         {
-
             return x + y;
         }
-        double sub(double x, double y)
+        static double sub(double x, double y)
         {
-
             return x - y;
         }
-        double mul(double x, double y)
+        static double div(double x, double y)
         {
-
-            return x * y;
-        }
-        double div(double x, double y)
-        {
-
+            if (y == 0)
+            {
+                Console.WriteLine("На ноль делить нельзя");
+                return 0;
+            }
+            else 
             return x / y;
         }
-        double pow(double x, double y)
+        static double mul(double x, double y)
         {
-
             return x * y;
         }
+        static double pow(double x, double y)
+        {
+            return Math.Pow (x,y);
+            
+        }
     }
-
 }
